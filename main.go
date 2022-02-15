310aef6ebc66ae7545c8636156757ae3d9d7db1c
Brandon Goh <gohkinshaunb@vmware.com>
2022-02-15 21:28:48 +0800

    updated testing

--- main.go
+++ main.go
@@ -1,42 +1,146 @@
 package main
 
 import (
+	"bytes"
+	"compress/gzip"
+	"encoding/json"
 	"fmt"
 	"io"
+
 	"log"
 	"net/http"
 	"net/url"
+
+	"github.com/gorilla/mux"
 )
 
-func main() {
-	demoURL, err := url.Parse("https://api.coinbase.com/v2/prices/spot?currency=USD")
-	fmt.Println(demoURL.Host, demoURL.Scheme, demoURL.Path, demoURL.RawQuery)
+const (
+	defaultAPIURL = "https://api.coinbase.com/v2/prices/spot"
+)
+
+type DataResponse struct {
+	Data struct {
+		Base     string `json:"base"`
+		Currency string `json:"currency"`
+		Amount   string `json:"amount"`
+	} `json:"data"`
+}
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+type application struct {
+	apiURL string
+}
+
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+	return false
+}
+
+func (a *application) spotPriceHandler(rw http.ResponseWriter, req *http.Request) {
+	// Parse apiURL
+	apiURL, err := url.Parse(a.apiURL)
 	if err != nil {
-		log.Fatal(err)
+		rw.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(rw).Encode(ErrorResponse{fmt.Sprintf("Invalid URL: %v, Error: %v", a.apiURL, err)})
+		return
 	}
 
-	proxy := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		req.Host = demoURL.Host
-		req.URL.Host = demoURL.Host
-		req.URL.Scheme = demoURL.Scheme
-		req.URL.Path = demoURL.Path
-		req.URL.RawQuery = demoURL.RawQuery
-		req.RequestURI = ""
-		resp, err := http.DefaultClient.Do(req)
+	// Validate currency
+	vars := mux.Vars(req)
+	currency := vars["currency"]
+	acceptedCurrencies := []string{"EUR", "GBP", "USD", "JPY"}
+	if ok := contains(acceptedCurrencies, currency); !ok {
+		rw.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(rw).Encode(ErrorResponse{`Currency not accepted, please choose between "EUR, GBP, USD, JPY"`})
+		return
+	}
+
+	// Copy request
+	rawQuery := "currency=" + currency
+	req.Host = apiURL.Host
+	req.URL.Host = apiURL.Host
+	req.URL.Scheme = apiURL.Scheme
+	req.URL.Path = apiURL.Path
+	req.URL.RawQuery = rawQuery
+	req.RequestURI = ""
+	req.Header.Add("Accept-Encoding", "gzip")
+
+	// Make request
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		rw.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(rw, err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Read body of response into bytes
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		rw.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(rw, err)
+		return
+	}
+
+	// Check that server actually sent compressed data
+	buf := bytes.NewBuffer(bodyBytes)
+	var reader io.ReadCloser
+	switch resp.Header.Get("Content-Encoding") {
+	case "gzip":
+		reader, err = gzip.NewReader(buf)
 		if err != nil {
+			fmt.Println("test")
 			rw.WriteHeader(http.StatusInternalServerError)
 			fmt.Fprint(rw, err)
 			return
 		}
-		rw.WriteHeader(resp.StatusCode)
-		_, err = io.Copy(rw, resp.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-	})
+	default:
+		reader = resp.Body
+	}
 
-	err = http.ListenAndServe(":8080", proxy)
+	// Decode json from the io.Reader
+	var response DataResponse
+	if err := json.NewDecoder(reader).Decode(&response); err != nil && err != io.EOF {
+		rw.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(rw, err)
+		return
+	}
+
+	// Write response
+	rw.WriteHeader(resp.StatusCode)
+	err = json.NewEncoder(rw).Encode(response)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+}
+
+func (*application) healthCheckHandler(rw http.ResponseWriter, req *http.Request) {
+	rw.WriteHeader(http.StatusOK)
+	rw.Header().Set("Content-Type", "application/json")
+	io.WriteString(rw, `{"alive": true}`)
+}
+
+func (a *application) handler() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/health", http.HandlerFunc(a.healthCheckHandler)).Methods("GET")
+	r.HandleFunc("/{currency}", http.HandlerFunc(a.spotPriceHandler)).Methods("GET")
+	return r
+}
+
+func main() {
+	a := application{
+		apiURL: defaultAPIURL,
+	}
+	r := a.handler()
+
+	log.Println("Server starting on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
--- main_test.go
+++ main_test.go
@@ -1,10 +1,179 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/require"
 )
 
-func TestPass(t *testing.T) {
-	fmt.Println("Placeholder")
+func TestHealthCheckHandler(t *testing.T) {
+	// Create a request to pass to our handler
+	req, err := http.NewRequest("GET", "/health-check", nil)
+	if err != nil {
+		t.Fatalf("Could not create request: %v", err)
+	}
+
+	// Create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rec := httptest.NewRecorder()
+
+	a := application{
+		apiURL: defaultAPIURL,
+	}
+
+	// Call ServeHTTP method to directly and pass in our Request and ResponseRecorder.
+	handler := http.HandlerFunc(a.healthCheckHandler)
+	handler.ServeHTTP(rec, req)
+
+	// Check the status code is what we expect.
+	if status := rec.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+	expected := `{"alive": true}`
+	if rec.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rec.Body.String(), expected)
+	}
+}
+
+func TestSpotPriceHandler(t *testing.T) {
+
+	t.Run("Happy path", func(t *testing.T) {
+		testServer := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			rw.WriteHeader(http.StatusOK)
+			_, err := rw.Write([]byte(`{"data":{"base":"BTC","currency":"USD","amount":"44259.64"}}`))
+			require.NoError(t, err)
+		}))
+		defer testServer.Close()
+
+		req, err := http.NewRequest("GET", "/:currency", nil)
+		if err != nil {
+			t.Fatalf("Could not create request: %v", err)
+		}
+
+		vars := map[string]string{
+			"currency": "USD",
+		}
+
+		req = mux.SetURLVars(req, vars)
+		rec := httptest.NewRecorder()
+
+		a := application{
+			apiURL: testServer.URL,
+		}
+
+		handler := http.HandlerFunc(a.spotPriceHandler)
+		handler.ServeHTTP(rec, req)
+
+		// Check the status code is what we expect.
+		if status := rec.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+		}
+
+		var response DataResponse
+		if err := json.NewDecoder(rec.Body).Decode(&response); err != nil && err != io.EOF {
+			t.Errorf("Expected json decoding success; got %v", err)
+		}
+	})
+
+	t.Run("Currency not implemented", func(t *testing.T) {
+		testServer := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			rw.WriteHeader(http.StatusOK)
+			_, err := rw.Write([]byte(`{"data":{"base":"BTC","currency":"USD","amount":"44259.64"}}`))
+			require.NoError(t, err)
+		}))
+		defer testServer.Close()
+
+		req, err := http.NewRequest("GET", "/:currency", nil)
+		if err != nil {
+			t.Fatalf("Could not create request: %v", err)
+		}
+
+		vars := map[string]string{
+			"currency": "MYR",
+		}
+
+		req = mux.SetURLVars(req, vars)
+		rec := httptest.NewRecorder()
+
+		a := application{
+			apiURL: testServer.URL,
+		}
+
+		handler := http.HandlerFunc(a.spotPriceHandler)
+		handler.ServeHTTP(rec, req)
+
+		// Check the status code is what we expect.
+		if status := rec.Code; status != http.StatusBadRequest {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
+		}
+
+		var response DataResponse
+		if err := json.NewDecoder(rec.Body).Decode(&response); err != nil && err != io.EOF {
+			t.Errorf("Expected json decoding success; got %v", err)
+		}
+	})
+
+	t.Run("Invalid URL", func(t *testing.T) {
+		testServer := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			rw.WriteHeader(http.StatusOK)
+			_, err := rw.Write([]byte(`{"data":{"base":"BTC","currency":"USD","amount":"44259.64"}}`))
+			require.NoError(t, err)
+		}))
+		defer testServer.Close()
+
+		req, err := http.NewRequest("GET", "/:currency", nil)
+		if err != nil {
+			t.Fatalf("Could not create request: %v", err)
+		}
+
+		vars := map[string]string{
+			"currency": "MYR",
+		}
+
+		req = mux.SetURLVars(req, vars)
+		rec := httptest.NewRecorder()
+
+		a := application{
+			apiURL: "%_+",
+		}
+
+		handler := http.HandlerFunc(a.spotPriceHandler)
+		handler.ServeHTTP(rec, req)
+
+		fmt.Println()
+		// Check the status code is what we expect.
+		if status := rec.Code; status != http.StatusBadRequest {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
+		}
+
+		var response DataResponse
+		if err := json.NewDecoder(rec.Body).Decode(&response); err != nil && err != io.EOF {
+			t.Errorf("Expected json decoding success; got %v", err)
+		}
+	})
+}
+
+func TestRouting(t *testing.T) {
+	a := application{
+		apiURL: defaultAPIURL,
+	}
+	srv := httptest.NewServer(a.handler())
+	defer srv.Close()
+
+	res, err := http.Get(fmt.Sprintf("%s/USD", srv.URL))
+	if err != nil {
+		t.Fatalf("Could not send GET request: %v", err)
+	}
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Expected status OK; got %v", res.Status)
+	}
 }
