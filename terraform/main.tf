b08b9f4e7e6090018360e678ffb0decc17a3ccea
Brandon Goh <gohkinshaunb@vmware.com>
2022-02-16 23:49:39 +0800

    updated terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+  required_version = ">=0.14.9"
+
+  backend "s3" {
+    bucket = "reverse-proxy-terraform"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
